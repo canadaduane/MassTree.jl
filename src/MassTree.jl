e5207d4de4af73829ac0de426409851d3eb35132
Duane Johnson <duane.johnson@gmail.com>
2014-10-06 21:55:31 -0600

    Get build working on OSX

--- src/MassTree.jl
+++ src/MassTree.jl
@@ -1,15 +1,19 @@
 module MassTree
 
-  const _libmasstree = "libmasstree"
+  export MTDict
+
+  if isfile(joinpath(Pkg.dir("MassTree"), "deps", "deps.jl"))
+    include("../deps/deps.jl")
+  else
+    error("MassTree not properly installed. Please run Pkg.build(\"MassTree\") and restart julia")
+  end
 
   typealias MasstreeWrapper Ptr{Void}
 
-  export MassTree
-
-  type MassTree{Tk, Tv}
+  type MTDict{Tk, Tv}
     masstree::MasstreeWrapper
     gc_store::ObjectIdDict
-    function MassTree()
+    function MTDict()
       masstree = mt_create()
       mt = new(masstree, ObjectIdDict())
       finalizer(mt, mt_destroy)
@@ -17,22 +21,22 @@ module MassTree
     end
   end
 
-  function mt_destroy(mt::MassTree)
-    ccall((:mt_destroy, _libmasstree), Void, (Ptr{Void},), mt.masstree)
+  function mt_destroy(mt::MTDict)
+    ccall((:mt_destroy, libmasstree), Void, (Ptr{Void},), mt.masstree)
   end
 
   function mt_create()
-    ccall((:mt_create, _libmasstree), Ptr{Void}, ())
+    ccall((:mt_create, libmasstree), Ptr{Void}, ())
   end
 
   # String : String
 
-  function mt_put(mt::MassTree{String, String}, key::String, value::String)
-    ccall((:mt_put_str_str, _libmasstree), Void, (Ptr{Void}, Ptr{Uint8}, Ptr{Uint8}), mt.masstree, key, value)
+  function mt_put(mt::MTDict{String, String}, key::String, value::String)
+    ccall((:mt_put_str_str, libmasstree), Void, (Ptr{Void}, Ptr{Uint8}, Ptr{Uint8}), mt.masstree, key, value)
   end
 
-  function mt_get(mt::MassTree{String, String}, key::String)
-    value = ccall((:mt_get_str_str, _libmasstree), Ptr{Uint8}, (Ptr{Void}, Ptr{Uint8}), mt.masstree, key)
+  function mt_get(mt::MTDict{String, String}, key::String)
+    value = ccall((:mt_get_str_str, libmasstree), Ptr{Uint8}, (Ptr{Void}, Ptr{Uint8}), mt.masstree, key)
     if value == C_NULL
       None
     else
@@ -42,12 +46,12 @@ module MassTree
 
   # String : Integer
 
-  function mt_put(mt::MassTree{String, Integer}, key::String, value::Int32)
-    ccall((:mt_put_str_int, _libmasstree), Void, (Ptr{Void}, Ptr{Uint8}, Cuint), mt.masstree, key, convert(Cuint, value))
+  function mt_put(mt::MTDict{String, Integer}, key::String, value::Int32)
+    ccall((:mt_put_str_int, libmasstree), Void, (Ptr{Void}, Ptr{Uint8}, Cuint), mt.masstree, key, convert(Cuint, value))
   end
 
-  function mt_get(mt::MassTree{String, Integer}, key::String)
-    value = ccall((:mt_get_str_int, _libmasstree), Ptr{Uint8}, (Ptr{Void}, Ptr{Uint8}), mt.masstree, key)
+  function mt_get(mt::MTDict{String, Integer}, key::String)
+    value = ccall((:mt_get_str_int, libmasstree), Ptr{Uint8}, (Ptr{Void}, Ptr{Uint8}), mt.masstree, key)
     if value == typemax(Uint32)
       None
     else
@@ -57,12 +61,12 @@ module MassTree
 
   # Integer : String
 
-  function mt_put(mt::MassTree{Integer, String}, key::Int32, value::String)
-    ccall((:mt_put_int_str, _libmasstree), Void, (Ptr{Void}, Cuint, Ptr{Uint8}), mt.masstree, key, value)
+  function mt_put(mt::MTDict{Integer, String}, key::Int32, value::String)
+    ccall((:mt_put_int_str, libmasstree), Void, (Ptr{Void}, Cuint, Ptr{Uint8}), mt.masstree, key, value)
   end
 
-  function mt_get(mt::MassTree{Integer, String}, key::Int32)
-    value = ccall((:mt_get_int_str, _libmasstree), Ptr{Uint8}, (Ptr{Void}, Cuint), mt.masstree, key)
+  function mt_get(mt::MTDict{Integer, String}, key::Int32)
+    value = ccall((:mt_get_int_str, libmasstree), Ptr{Uint8}, (Ptr{Void}, Cuint), mt.masstree, key)
     if value == C_NULL
       None
     else
@@ -72,12 +76,12 @@ module MassTree
 
   # Integer : Integer
 
-  function mt_put(mt::MassTree{Integer, Integer}, key::Int32, value::Int32)
-    ccall((:mt_put_int_int, _libmasstree), Void, (Ptr{Void}, Cuint, Cuint), mt.masstree, key, value)
+  function mt_put(mt::MTDict{Integer, Integer}, key::Int32, value::Int32)
+    ccall((:mt_put_int_int, libmasstree), Void, (Ptr{Void}, Cuint, Cuint), mt.masstree, key, value)
   end
 
-  function mt_get(mt::MassTree{Integer, Integer}, key::Int32)
-    value = ccall((:mt_get_int_int, _libmasstree), Ptr{Uint8}, (Ptr{Void}, Cuint), mt.masstree, key)
+  function mt_get(mt::MTDict{Integer, Integer}, key::Int32)
+    value = ccall((:mt_get_int_int, libmasstree), Ptr{Uint8}, (Ptr{Void}, Cuint), mt.masstree, key)
     if value == typemax(Uint32)
       None
     else
@@ -86,7 +90,7 @@ module MassTree
   end
 
 
-  getindex(mt::MassTree, key) = mt_get(mt, key)
-  setindex!(mt::MassTree, value, key) = mt_put(mt, key, value)
+  getindex(mt::MTDict, key) = mt_get(mt, key)
+  setindex!(mt::MTDict, value, key) = mt_put(mt, key, value)
 
 end
